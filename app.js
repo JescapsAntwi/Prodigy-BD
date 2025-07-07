2b501f63598217adfca3445a7ef23bf12419b259
JescapsAntwi <antwijescaps1@gmail.com>
2025-07-07 11:52:52 +0000

    implemented redis caching

--- app.js
+++ app.js
@@ -495,3 +495,13 @@ redisClient.on('connect', () => {
     redisClient.quit();
     process.exit();
   });
+
+  // Add this before your routes
+app.use((req, res, next) => {
+    const start = Date.now();
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.log(`${req.method} ${req.originalUrl} - ${duration}ms`);
+    });
+    next();
+  });
